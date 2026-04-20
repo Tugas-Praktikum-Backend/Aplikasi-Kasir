c06067b08e6ab4258cd97ae3fdf62bfbdbc40dd3
Moses <lol>
2026-04-20 10:24:47 +0700

    Revamp customers database

--- src/modules/endpoints/customers/customers-endpoint.js
+++ src/modules/endpoints/customers/customers-endpoint.js
@@ -4,28 +4,24 @@ const db = require('../../database/database-manager').getDatabase("Customers")
 // Controller
 async function addCustomer(req, res, next){
     try {
-        const {
-            customer_name: customerName
-        } = req.body;
+         const customerName = req.body.customer_name?.trim().toLowerCase();
 
-        if(!customer_name) {
+        if(!customerName) {
             throw Error('Name is required');
         }
 
-        if (customerExists(customer_name)) {
+        if (await customerExists(customerName)) {
             throw Error('Customer already exists');
         }
-        
-        const transactions = [];
 
         const result = await db.create({
-            customer_name,
-            transactions
+            customerName
         });
+
         if(!result){
-            throw Error('Failed to fetch customers data');
+            throw Error('Failed to create customer');
         }
-        res.status(201).json({ message: "Successfully added new customer.", customer_name: result });
+        res.status(201).json({ message: `Successfully added new customer. Customer added: ${result.customerName}` });
     } catch(err){
         next(err);
     }
@@ -34,14 +30,14 @@ async function addCustomer(req, res, next){
 async function deleteCustomer(req, res, next){
     try {
         const id = req.params.id;
-        if((!id) || (id === ":id")){
+        if(!id){
             throw Error('Missing id parameter');
         }
         const result = await db.deleteOne({ _id: id });
-        if(!result){
+        if(result.deletedCount === 0){
             throw Error(`Failed to delete customer with id ${id}`);
         }
-        res.status(201).json({ message: `Successfully deleted customer with id ${id}`});
+        res.status(200).json({ message: `Successfully deleted customer with id ${id}`});
     } catch(err){
         next(err);
     }
@@ -50,10 +46,12 @@ async function deleteCustomer(req, res, next){
 async function getCustomers(req, res, next){
     try {
         const result = await db.find({});
-        if(!result){
-            throw Error('Failed to fetch customers data');
-        }
-        res.status(201).json({ customer_name: result, transactions: result });
+        const customers = result.map(c => ({
+            id: c._id,
+            customer_name: c.customerName,
+            payment_method: c.paymentMethod
+        }));
+        res.status(200).json({ customers  });
     } catch(err){
         next(err);
     }
@@ -62,11 +60,14 @@ async function getCustomers(req, res, next){
 async function getCustomer(req, res, next){
     try {
         const id = req.params.id;
-
-        if((!id) || (id === ":id")){
+        if(!id){
             throw Error('Missing id parameter');
         }
-        res.status(200).json({ customer_name: result, transactions: result });
+        const result = await db.findById(id);
+        if(!result){
+            throw Error('Customer not found');
+        }
+        res.status(200).json({ id: result._id, customer_name: result.customerName, payment_method: result.paymentMethod });
     } catch (err) {
         next(err);
     }
@@ -74,42 +75,143 @@ async function getCustomer(req, res, next){
 
 async function updateCustomer(req, res, next){
     try {
-        const {
-            customer_name: customerName
-        } = req.body;
+        const customerName = req.body.customer_name?.trim().toLowerCase();
 
         const id = req.params.id;
-        if((!id) || (id === ":id")){
+        if(!id) {
             throw Error('Missing id parameter');
         }
 
-        if(!customer_name) {
+        if(!customerName) {
             throw Error('Name is required');
         }
 
-        const result = await db.updateOne({ _id: id }, { $set: {customer_name}})
-        if(!result){
-            throw Error(`Failed to delete customer with id ${id}`);
+        const result = await db.updateOne({ _id: id }, { $set: {customerName}})
+        if(result.matchedCount === 0){
+            throw Error(`Failed to update customer with id ${id}`);
         }
-        res.status(201).json({ message: `Successfully updated customer with id ${id}`});
+        res.status(200).json({ message: `Successfully updated customer with id ${id}`});
     } catch(err){
         next(err);
     }
 }
 
+async function addPayment(req, res, next){
+    try{
+        const provider = req.body.provider?.trim().toUpperCase();
+        const id = req.params.id;
+
+        if(!provider){
+            throw Error('Provider is required');
+        }
+
+        const result = await db.updateOne(
+            { _id: id, "paymentMethod.provider": { $ne: provider } },
+            { $push: { paymentMethod: { provider, balance: 0 } } }
+        );
+
+        if(result.matchedCount === 0){
+            throw Error('Customer not found');
+        }
+
+        if(result.modifiedCount === 0){
+            throw Error('Payment already exists');
+        }
+
+        res.status(200).json({ message: 'Payment method added' });
+    }catch(err){
+        next(err)
+    }
+}
+
+async function topUp(req, res, next){
+    try{
+        const provider = req.body.provider?.trim().toUpperCase();
+        const amount = Number(req.body.amount);
+        const id = req.params.id;
+        
+        if(!provider){
+            throw Error('Provider is required');
+        }
+
+        const customer = await db.findById(id);
+        if(!customer){
+            throw Error('Customer not found');
+        }
+
+        const payment = customer.paymentMethod.find(p => p.provider === provider);
+        if(!payment){
+            throw Error('Payment method not found');
+        }
+
+        if(!amount || amount <= 0){
+            throw Error('Amount must be greater than 0');
+        }   
+        const result = await db.updateOne(
+            { _id: id, "paymentMethod.provider": provider },
+            { $inc: { "paymentMethod.$.balance": amount } }
+        );
+
+        const updated = await db.findById(id);
+        const updatedPayment = updated.paymentMethod.find(p => p.provider === provider);
+
+        if(result.matchedCount === 0){
+            throw Error('Customer or payment not found');
+        }
+        
+        res.status(200).json({ message: 'Top up success', balance: updatedPayment.balance });
+    }catch(err){
+        next(err)
+    }
+}
+
+async function deletePayment(req, res, next){
+    try{
+        const provider = req.body.provider?.trim().toUpperCase();
+        const id = req.params.id;
+
+        if(!provider){
+            throw Error('Provider is required');
+        }
+
+        const result = await db.updateOne(
+            { _id: id, "paymentMethod.provider": provider },
+            { $pull: { paymentMethod: { provider } } }
+        );
+
+       if(result.matchedCount === 0){
+            throw Error('Customer not found');
+        }
+
+        if(result.modifiedCount === 0){
+            throw Error(`Payment ${provider} not found`);
+        }
+
+        res.status(200).json({
+            message: `Payment ${provider} deleted successfully`
+        });
+    }catch(err){
+        next(err)
+    }
+}
 // Service
-function customerExists(customerName) {
-    const user = await db.findOne({ customerName });
+async function customerExists(customerName) {
+    const user = await db.findOne({ 
+        customerName: customerName.trim().toLowerCase()
+    });
     return !!user;
 }
+
 
 // Route
 module.exports = (app) => {
     route.post('/', addCustomer);
     route.get('/', getCustomers);
-    route.get('/:id', getCustomer),
-    route.get('/:id/transactions', getCustomerTransactions),
+    route.get('/:id', getCustomer);
     route.put('/:id', updateCustomer);
+    route.put('/:id/topup', topUp);
+    route.put('/:id/payment', addPayment);
+    route.delete('/:id/payment', deletePayment);
     route.delete('/:id', deleteCustomer);
 
     app.use('/customers', route);
