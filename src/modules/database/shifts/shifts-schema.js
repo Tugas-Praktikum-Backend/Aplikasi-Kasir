66dfb46e757cbd0e1b1906fb6ef3984e06cf7b11
Moses <mosesmarvian@gmail.com>
2026-04-20 12:55:58 +0700

    Updated shifts

--- src/modules/database/shifts/shifts-schema.js
+++ src/modules/database/shifts/shifts-schema.js
@@ -2,7 +2,7 @@ module.exports = (db) => (
     {
         id: "Shifts",
         model: db.model('Shifts', db.Schema({
-            employeeId: { type: db.Schema.Types.ObjectId, required: true },
+            employeeId: { type: db.Schema.Types.ObjectId, ref: 'Employee', required: true },
             startTime: { type: Date, default: Date.now },
             endTime: { type: Date, default: null }
         }))
