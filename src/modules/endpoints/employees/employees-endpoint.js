c1cd1dc3801b7264a34e324ac99ccf89a83f387d
FresherGAMING <reyhan01abcde@gmail.com>
2026-04-22 11:49:24 +0700

    Setup Token

--- src/modules/endpoints/employees/employees-endpoint.js
+++ src/modules/endpoints/employees/employees-endpoint.js
@@ -1,5 +1,6 @@
 const route = require('express').Router();
 const db = require('../../database/database-manager').getDatabase('Employees');
+const { newToken } = require('../../middleware/middleware')
 
 async function addEmployee(req, res, next) {
   try {
@@ -90,12 +91,40 @@ async function deleteEmployee(req, res, next) {
   }
 }
 
+async function loginEmployee(req, res, next){
+  try {
+    if(!req.body){
+      throw Error('employeeName and employeePassword are required');
+    }
+    const { employeeName, employeePassword } = req.body;
+    if (!employeeName || !employeePassword) {
+      throw Error('employeeName and employeePassword are required');
+    }
+
+    const result = await db.find({ employeeName, employeePassword });
+    if(!result || result.length <= 0){
+      return res.status(401).json({
+        message: `Authorization failed`,
+      });
+    }
+
+    const token = newToken(result[0].employeeName);
+    return res.status(200).json({
+      message: `Authorization successfully`,
+      token: token
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
 module.exports = (app) => {
   route.post('/', addEmployee);
   route.get('/', getEmployees);
   route.get('/:id', getEmployee);
   route.put('/:id', updateEmployee);
   route.delete('/:id', deleteEmployee);
+  route.post('/login', loginEmployee);
 
   app.use('/employees', route);
 };
