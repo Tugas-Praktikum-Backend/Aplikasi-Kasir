90ecf1ab77707b3c9f98b871044898c136fc6453
mosesmarvian <mosesmarvian@gmail.com>
2026-04-22 10:46:51 +0700

    product update

--- src/modules/database/product/product-schema.js
+++ src/modules/database/product/product-schema.js
@@ -2,13 +2,10 @@ module.exports = (db) => ({
   id: 'Products',
   model: db.model(
     'Products',
-    db.Schema(
-      {
-        productId: { type: String, required: true },
-        productName: { type: String, required: true },
-        productPrice: { type: Number, required: true },
-        productStock: { type: Number, default: 0 },
-      },
-    )
+    db.Schema({
+      productName: { type: String, required: true },
+      productPrice: { type: Number, required: true },
+      productStock: { type: Number, default: 0 },
+    })
   ),
 });
